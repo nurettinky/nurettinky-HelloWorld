4cfab7cf862d3e9bde87e6a144cf764c08142947
Nurettin Kaya <nurettinky1@gmail.com>
2023-02-24 21:24:55 +0000

    System.out.println("Hello, World!");

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -5,13 +5,10 @@ import org.junit.Test;
 /**
  * DO NOT change anything within this test file. All code should be written in src/main/java/Lab.java
  */public class HelloWorldTest {
- */  Lab hw = new Lab();
+   Lab hw = new Lab();
 
     /**
      * Method sayHello() must return "Hello, World!". The trim method will remove any extra spaces or newlines at the end.
      */@Test
-   Class Main {
-    public static void main(String[] args) {
-        System.out.print("Hello,world!");
-    }
-   }
+
+     System.out.println("Hello, World!");
