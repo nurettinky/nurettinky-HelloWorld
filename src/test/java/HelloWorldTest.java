430239c90586fae9e10dfc386fd1c9fae2e9fa53
Nurettin Kaya <nurettinky1@gmail.com>
2023-02-24 21:59:19 +0000

    Competed Hello World

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -9,9 +9,8 @@ import org.junit.Test;
     /**
      * Method sayHello() must return "Hello, World!". The trim method will remove any extra spaces or newlines at the end.
      */@Test
-
-     Class Main {
-      public class HelloWorldTest {
-        System.out.println("Hello, world!");
-      }
-    }
+     Class Main{
+        public class main{
+            System.out.println("Hello, world!");
+        }
+     }
